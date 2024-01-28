4f9ce61eb06bf7d4218add88a20b6d2fc494645c
Mariia Davydenko <mary.davydenko1998@gmail.com>
2024-01-29 01:15:48 +0200

    first commit

---
+++ homework_3/homework_3_task_1.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+def get_days_from_today(date):
+    current_date = datetime.now()
+    while True:
+        try:
+            users_date = datetime.strptime(date, '%Y-%m-%d')
+            return f'Кількість днів між датами: {(users_date-current_date).days}'
+        except Exception:
+            date=input(f'{date} не відповідає формату дати РРРР-ММ-ДД, введіть іншу дату: ') 
+
+print (get_days_from_today(input('Введіть дату у форматі РРРР-ММ-ДД: ')))
+# шось
