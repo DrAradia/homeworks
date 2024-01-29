d047916d5342f1097749fb04b52d84da8b169b7c
Mariia Davydenko <mary.davydenko1998@gmail.com>
2024-01-29 14:06:12 +0200

    task_2

---
+++ homework_3/homework_3_task_2.py
@@ -0,0 +1,9 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    if (min < 1) or (max > 1000) or (min > max) or (quantity > (max - min + 1)):
+        return []
+    uniq_numbers = random.sample(range(min, max+1), quantity)
+    return sorted(uniq_numbers)
+numbers_print = get_numbers_ticket(2, 999, 5)
+print(f"Ваші лотерейні числа: {numbers_print}")
