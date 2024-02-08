8dd1000b4539e155f2928481a22c66ec6d773e21
Mariia Davydenko <mary.davydenko1998@gmail.com>
2024-02-08 19:04:36 +0200

    mentor

--- goit-algo-hw-04/task_1/hw_4_task_1.py
+++ goit-algo-hw-04/task_1/hw_4_task_1.py
@@ -1,12 +1,25 @@
-def total_salary(path):
-    with open(path, "r") as fh:
-        salary = [float(el.strip().split(',')[1]) for el in fh.readlines() if el]
-        return (sum(salary), sum(salary)/len(salary)) #(загальна сума зарплат, середня заробітна плата)
+# def total_salary(path):
+#     with open(path, "r") as fh:
+#         salary = [float(el.strip().split(',')[1]) for el in fh.readlines() if el]
+#         return (sum(salary), sum(salary)/len(salary)) #(загальна сума зарплат, середня заробітна плата)
     
-try:    
-    total, average = total_salary("./file/salary_file.txt")
-    print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
-except FileNotFoundError:
-    print("Не вдалося знайти файл з зарплатами. Вкажіть правильний шлях")
-except Exception as e:
-    print(f'Помилка {e}')
+# try:    
+#     total, average = total_salary("./file/salary_file.txt")
+#     print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+# except FileNotFoundError:
+#     print("Не вдалося знайти файл з зарплатами. Вкажіть правильний шлях")
+# except Exception as e:
+#     print(f'Помилка {e}')
+
+def total_salary(path):
+    try:
+        with open(path, "r") as fh:
+            salary = [float(el.strip().split(',')[1]) for el in fh.readlines() if el]
+            return (sum(salary), sum(salary)/len(salary)) #(загальна сума зарплат, середня заробітна плата)
+    except FileNotFoundError:
+        print("Не вдалося знайти файл з зарплатами. Вкажіть правильний шлях")
+    except Exception as e:
+        print(f'Помилка {e}')
+
+total, average = total_salary("./file/salary_file.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
